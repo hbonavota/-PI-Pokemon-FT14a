eee9e9d7a5acb8ecfe6988ee4787b6ce47e75221
¨hbonavota¨ <hbonavota@gmail.com>
2021-07-12 19:58:41 +0200

    starting with the Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
+import React from "react";
 import './App.css';
+import Search from "./components/Search/Search.js"
+import Landing from "./components/Landing/Landing.js"
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <React.Fragment>
+      <Landing />
+      <Route exact path="/home" component={Search} />
+        <div className="App">
+        </div>
+    </React.Fragment>
   );
 }
+
 
 export default App;
---
+++ client/src/components/Search/Search.js
@@ -0,0 +1,11 @@
+function Search() {
+    return (
+          <div >
+          <h1>Henry Pokemon Buscador</h1>
+          </div>
+    );
+  }
+
+
+  export default Search;
+  
